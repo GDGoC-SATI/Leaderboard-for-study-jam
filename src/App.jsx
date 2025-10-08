78a1da148bbfafea88315302f1bbd22b01a4b3b6
Deepak kumar gupta <deepak28it022@satiengg.in>
2025-10-08 19:21:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home.jsx";
-import Login from "./pages/Login.jsx";
+
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
+    
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
